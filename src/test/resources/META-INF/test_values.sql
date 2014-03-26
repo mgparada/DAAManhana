88e344459c0458340b706e75c0270fb2c70d1e4e
mgparada <mgparada@esei.uvigo.es>
2014-03-26 14:21:53 +0100

    Script inserts for Articles entity

--- src/test/resources/META-INF/test_values.sql
+++ src/test/resources/META-INF/test_values.sql
@@ -1,7 +1,9 @@
-insert into articulos (fechaAlta, nombre) values ("2014-03-26 00:34:04", "articulo1");
-insert into articulos (fechaAlta, nombre) values ("2015-03-26 00:34:04", "batman");
-insert into articulos (fechaAlta, nombre) values ("2014-02-26 00:34:04", "spiderman");
-insert into articulos (fechaAlta, nombre) values ("2014-03-09 00:34:04", "algo");
-insert into articulos (fechaAlta, nombre) values ("2012-03-26 00:34:04", "otro articulo");
-insert into articulos (fechaAlta, nombre) values ("2014-12-26 00:34:04", "no se me ocurren mas nombres");
-insert into articulos (fechaAlta, nombre) values ("2011-01-26 00:34:04", "el último");
+insert into articles (releaseDate, name, description) 
+		values ("1972-03-26 00:34:04", "El padrino", "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."),
+			   ("1994-03-26 21:34:23", "Cadena Perpetua", "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."),
+			   ("1974-03-26 20:40:23", "El padrino. Parte II", "The early life and career of Vito Corleone in 1920s New York is portrayed while his son, Michael, expands and tightens his grip on his crime syndicate stretching from Lake Tahoe, Nevada to pre-revolution 1958 Cuba."),
+			   ("2008-03-26 19:25:12", "El caballero oscuro", "When Batman, Gordon and Harvey Dent launch an assault on the mob, they let the clown out of the box, the Joker, bent on turning Gotham on itself and bringing any heroes down to his level."),
+			   ("1994-05-13 14:27:23", "Cadena Perpetua", "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."),
+			   ("1994-11-03 11:44:12", "Pulp Fiction", "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption."),
+			   ("1993-01-26 03:32:43", "12 Hombres sin Piedad", "A dissenting juror in a murder trial slowly manages to convince the others that the case is not as obviously clear as it seemed in court."),
+			   ("1957-02-14 18:14:25", "La lista de Schindler", "n Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.");
